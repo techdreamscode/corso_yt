3d7db145b23f5f52e5dd2cb1915552653d807615
techdreamscode <94684128+techdreamscode@users.noreply.github.com>
2021-11-19 12:02:49 +0100

    Add files via upload

---
+++ Funzione_print.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 12 18:22:09 2021
+
+@author: danic
+"""
+
+# Somma = a + b
+# Sottrazione = a - b
+# Moltiplicazione = a * b
+# Divisione = a / b
+# Elevamento a potenza = a ** b
+# Divisione senza resto = a // b
+
+print('Inserisci quel che vuoi :)')
+
+
+
+
